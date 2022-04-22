cb6f07e99663267176d07c96ebec4bafeccfd002
EC2 Default User <ec2-user@ip-172-31-80-39.ec2.internal>
2022-04-22 01:58:16 +0000

    updated code

--- factorial.py
+++ factorial.py
@@ -18,27 +18,28 @@ def main():
     try:
         # change input to int
         user_num_as_int = int(user_num_as_string)
-        # calculate the factore
-        while True:
-            loop_counter = loop_counter + 1
-            factor = factor * loop_counter
-            print("Tracking {} times through loop.".format(loop_counter))
-            print("")
-            # print("The sum of the numbers from 0 to {} is:
-            # {}.".format(user_num_as_int, sum))
-
-            if loop_counter >= user_num_as_int:
-                break
-            # display 1 if input is zero
-            if user_num_as_int == 0:
-                print("user_num_as_int !", 1)
         if user_num_as_int < 0:
             print("Input was not a positive number.")
-        print("{}! = {}". format(user_num_as_int, factor))
-        # display invalid input
+            exit()
+        else:
+            # calculate the factore
+            while True:
+                loop_counter = loop_counter + 1
+                print("Tracking {} times through loop.".format(loop_counter))
+                factor = factor * loop_counter
+                print("")
+                if loop_counter >= user_num_as_int:
+                    break
+       
+            
+    # display invalid input
     except Exception:
         print("")
-        print("input is not a integer.")
+        print("input is not an integer.")
+        exit()
+        
+    print("{}! = {}". format(user_num_as_int, factor))
+
 
 
 if __name__ == "__main__":
